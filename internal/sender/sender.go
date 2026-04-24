fe2c48e552f7b09c912d0385334676404aa1eaff
sanenchen <srcenchen@gmail.com>
2026-04-24 17:10:43 +0800

    fix(main): 修复 sender 文件无法打开情况

--- internal/sender/sender.go
+++ internal/sender/sender.go
@@ -10,7 +10,7 @@ func Start(filePath string) {
 	// 启动sender服务端（主节点）
 	s := gsp_sdk.NewGspSession(":18080")
 	s.Start()
-	f, err := os.Open("filePath")
+	f, err := os.Open(filePath)
 	if err != nil {
 		slog.Error(filePath + "不存在")
 		os.Exit(0)
