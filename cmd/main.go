f4de28cfd3e33dc0aacc0a85f99d30878aaefe35
sanenchen <srcenchen@gmail.com>
2026-04-24 16:48:01 +0800

    feat(main): 实现一对多，单文件下载的demo

--- cmd/main.go
+++ cmd/main.go
@@ -8,22 +8,27 @@ import (
 )
 
 var (
-	mode string
+	mode       string
+	filePath   string
+	senderAddr string
 )
 
 func init() {
 	flag.StringVar(&mode, "mode", "sender", "模式选择，receiver/sender")
+	flag.StringVar(&filePath, "file", "test.file", "文件路径")
+	flag.StringVar(&senderAddr, "senderAddr", "127.0.0.1:18080", "发送端地址，类似 192.168.1.10:18080")
 }
 func main() {
 	flag.Parse()
 	// 接收端模式和发送端模式
 	if mode == "receiver" {
 		log.Println("接收模式")
-		receiver.Start()
+		receiver.Start(senderAddr)
 	} else if mode == "sender" {
 		log.Println("发送模式")
-		sender.Start()
+		sender.Start(filePath)
 	} else {
 		panic("模式选择错误")
 	}
+	select {}
 }
--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -1,5 +1,40 @@
 package receiver
 
-func Start() {
+import (
+	"fmt"
+	"go-silver-core/internal/gsp_sdk"
+	"go-silver-core/pkg/chunk"
+	"net"
+	"os"
+)
 
+func Start(senderAddr string) {
+	conn, err := net.Dial("tcp", senderAddr)
+	if err != nil {
+		panic("发送端连接失败")
+	}
+	gspC := gsp_sdk.NewGspSdk(conn)
+	status, err := gspC.GetFileStatus()
+	if err != nil {
+		return
+	}
+	// 创建 文件
+	f, err := os.Create(status.FileName)
+	if err != nil {
+		panic("文件创建失败")
+	}
+	f.Truncate(status.FileSize)
+	ck := chunk.NewFileChunk(f)
+	for i := int64(0); i < status.ChunkNum; i++ {
+		fmt.Printf("下载 %d / %d 块中...\n", i+1, status.ChunkNum)
+		data, err := gspC.GetChunk(senderAddr, i)
+		if err != nil {
+			panic(err)
+		}
+		if err := ck.Save(i, data); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("下载完毕！")
+	os.Exit(0)
 }
